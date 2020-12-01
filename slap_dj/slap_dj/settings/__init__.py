a9dc7e99da5a18848aa9e000bbc8e06d94b5ff2a
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-12-02 02:42:20 +0700

    Fix BASE_DIR

--- slap_dj/slap_dj/settings/__init__.py
+++ slap_dj/slap_dj/settings/__init__.py
@@ -14,8 +14,7 @@ from dj_database_url import parse as db_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
