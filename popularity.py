ba974c4b9f223b2b3990b69e272ba415f9af318b
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-12-02 04:11:38 +0700

    Add parameterized logistic function

--- popularity.py
+++ popularity.py
@@ -1,10 +1,23 @@
 import numpy as np
 from scipy import integrate
 from scipy import special
+from functools import partial
 
 
 def logistic(x):
     return special.expit(x)
+
+
+def logistic2(L, k, x0, x):
+    """
+
+    Args:
+        L: the curve's maximum value
+        k: the logistic growth rate or steepness of the curve
+        x0: the x value of the sigmoid's midpoint
+        x: the input
+    """
+    return np.divide(L, np.add(1, np.exp(np.negative(np.multiply(k, np.subtract(x, x0))))))
 
 
 def tanh(x):
@@ -28,7 +41,18 @@ def func1(x):
 
 
 if __name__ == '__main__':
-    f = logistic
-    print(f.__name__)
-    for i in range(1, 11):
-        print(i, f(i))
+    for f in [
+        logistic,
+        partial(logistic2, 1, 1, 0),
+        # tanh,
+        # arctan,
+        # erf,
+        # func1,
+    ]:
+        try:
+            print(f.__name__)
+        except AttributeError:
+            print(f.func.__name__)
+        for i in range(1, 16):
+            print(i, f(i))
+        print()
