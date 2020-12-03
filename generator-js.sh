aec2f9b7fe22bdbc4edbe833efd5f038d380ffca
T. Bunratta <tharathorn.bu@ku.th>
2020-12-04 00:07:40 +0700

    Create a REST client generator shell script for JS

---
+++ generator-js.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+java -jar autogen/openapi-generator-cli-4.3.1.jar generate \
+-i "openapi/slap-api.yaml" --additional-properties=projectName=slap-client \
+-o slap-client -g javascript
+cd slap-client || exit
+npm install
+npm run build
+cd ..
+cp -rf slap-client slap-vue/node_modules/
+rm -r slap-client
