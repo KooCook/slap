8b8d03e1a3b4c3c93415797a4cc06e57abf266e3
T. Bunratta <tharathorn.bu@ku.th>
2020-12-04 19:31:13 +0700

    Add Vue Store

--- slap-vue/src/main.js
+++ slap-vue/src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
