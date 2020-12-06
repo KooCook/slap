193c67d623e6870020a41d2c68535dd89eeba9c9
T. Bunratta <tharathorn.bu@ku.th>
2020-12-06 13:05:00 +0700

    Add Word Cache and Frequency update functions

--- slap_dj/app/models/word.py
+++ slap_dj/app/models/word.py
@@ -1,13 +1,50 @@
 from django.db import models
 
-from slap_dj.app.models import Song
+from repetition import get_bow_dataframe
+from services.genius import tokenize_words
+from .base import Song
 
 
 class WordCache(models.Model):
-    word = models.CharField(max_length=289)
-    frequency = models.IntegerField()
+    word = models.CharField(max_length=289, unique=True)
+    occurs_in_song = models.ManyToManyField(Song, through='WordOccurrenceInSong')
 
 
 class WordOccurrenceInSong(models.Model):
     word = models.ForeignKey(WordCache, on_delete=models.CASCADE)
     appears_in = models.ForeignKey(Song, on_delete=models.CASCADE)
+    frequency = models.IntegerField()
+
+    @classmethod
+    def update_all_songs_word_frequency(cls, skip=True):
+        for s in Song.objects.all():
+            try:
+                cls.objects.get(appears_in=s)
+                if skip:
+                    continue
+            except cls.DoesNotExist:
+                pass
+            cls.update_song_word_frequency(s)
+
+    @classmethod
+    def update_song_word_frequency(cls, song: Song):
+        tokenized = tokenize_words(song.lyrics)
+        df = get_bow_dataframe(tokenized)
+        freq = list(df['freq'])
+        words = list(df['word'])
+        for w, f in zip(words, freq):
+            cls.upsert(w, song, f)
+
+    @classmethod
+    def upsert(cls, word: str, song: Song, frequency: float) -> 'WordOccurrenceInSong':
+        try:
+            w = WordCache.objects.get(word__iexact=word)
+        except WordCache.DoesNotExist:
+            w = WordCache.objects.create(word=word)
+        try:
+            wo = cls.objects.get(word=w, appears_in=song)
+            wo.frequency = frequency
+            wo.save()
+            return wo
+        except WordOccurrenceInSong.DoesNotExist:
+            return cls.objects.create(word=w, appears_in=song, frequency=frequency)
