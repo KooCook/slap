43beffcb1ecb2de529435bcb000bf9ba1986a62d
T. Bunratta <tharathorn.bu@ku.th>
2020-12-10 13:37:50 +0700

    Move ArtistRoleInSong to the internal class of ArtistInSong

--- slap_dj/app/models/artists.py
+++ slap_dj/app/models/artists.py
@@ -5,14 +5,12 @@ from django.db import models
 from .base import Artist, Song
 
 
-class ArtistRoleInSong(models.TextChoices):
-    Primary = "primary"
-    Secondary = "secondary"
-    Collaborator = "collaborator"
-    Featured = "featured"
-
-
 class ArtistInSong(models.Model):
+    class ArtistRole(models.TextChoices):
+        Primary = "primary"
+        Secondary = "secondary"
+        Collaborator = "collaborator"
+        Featured = "featured"
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    role = models.CharField(max_length=64, choices=ArtistRoleInSong.choices)
+    role = models.CharField(max_length=64, choices=ArtistRole.choices)
