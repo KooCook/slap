de1f8675f05d6af83078ad3fc702c742ad5d8428
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-12-11 15:28:30 +0700

    Clean & Fix style in models/artist.py

--- slap_dj/app/models/artists.py
+++ slap_dj/app/models/artists.py
@@ -1,16 +1,16 @@
-from enum import Enum
-
 from django.db import models
 
 from .base import Artist, Song
 
 
 class ArtistInSong(models.Model):
+
     class ArtistRole(models.TextChoices):
         Primary = "primary"
         Secondary = "secondary"
         Collaborator = "collaborator"
         Featured = "featured"
+
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     role = models.CharField(max_length=64, choices=ArtistRole.choices)
