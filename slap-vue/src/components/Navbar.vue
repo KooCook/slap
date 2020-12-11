976ac646c136867adad405143b33ab592e17b62c
T. Bunratta <tharathorn.bu@ku.th>
2020-12-11 16:31:15 +0700

    Comment out the unnecessary section of Navbar

--- slap-vue/src/components/Navbar.vue
+++ slap-vue/src/components/Navbar.vue
@@ -12,34 +12,34 @@
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input
-            size="sm"
-            class="mr-sm-2"
-            placeholder="Search"
-          ></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit"
-            >Search</b-button
-          >
-        </b-nav-form>
+<!--      <b-navbar-nav class="ml-auto">-->
+<!--        <b-nav-form>-->
+<!--          <b-form-input-->
+<!--            size="sm"-->
+<!--            class="mr-sm-2"-->
+<!--            placeholder="Search"-->
+<!--          ></b-form-input>-->
+<!--          <b-button size="sm" class="my-2 my-sm-0" type="submit"-->
+<!--            >Search</b-button-->
+<!--          >-->
+<!--        </b-nav-form>-->
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
+<!--        <b-nav-item-dropdown text="Lang" right>-->
+<!--          <b-dropdown-item href="#">EN</b-dropdown-item>-->
+<!--          <b-dropdown-item href="#">ES</b-dropdown-item>-->
+<!--          <b-dropdown-item href="#">RU</b-dropdown-item>-->
+<!--          <b-dropdown-item href="#">FA</b-dropdown-item>-->
+<!--        </b-nav-item-dropdown>-->
 
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
+<!--        <b-nav-item-dropdown right>-->
+<!--          &lt;!&ndash; Using 'button-content' slot &ndash;&gt;-->
+<!--          <template #button-content>-->
+<!--            <em>User</em>-->
+<!--          </template>-->
+<!--          <b-dropdown-item href="#">Profile</b-dropdown-item>-->
+<!--          <b-dropdown-item href="#">Sign Out</b-dropdown-item>-->
+<!--        </b-nav-item-dropdown>-->
+<!--      </b-navbar-nav>-->
     </b-collapse>
   </b-navbar>
 </template>
