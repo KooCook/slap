215ebced7f6071688c1dbddc4a0ecc42f9df1001
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-12-12 11:29:08 +0700

    Rename check_word_english_alpha() -> is_english_word_alpha() for clarity

--- slap_dj/app/views/kpop.py
+++ slap_dj/app/views/kpop.py
@@ -7,20 +7,16 @@ from app.models import Song
 from repetition import get_bow_dataframe
 
 
-def check_word_english_alpha(word: str) -> bool:
-    """
-    Checks whether the word is an English word or not
+def is_english_word_alpha(word: str) -> bool:
+    """Returns True when `word` is an English word. False otherwise.
 
     Args:
         word: Any given word
 
-    Returns:
-        Whether the word is an English word or not
-
     Examples:
-        >>> check_word_english_alpha("English")
+        >>> is_english_word_alpha("English")
         True
-        >>> check_word_english_alpha("영원한")
+        >>> is_english_word_alpha("영원한")
         False
     """
     return word.upper() != word.lower()
@@ -34,7 +30,7 @@ class KpopGenreView(APIView):
         eng_words_each = []
         for idx, song in enumerate(kpop_songs):
             tokenized = song.words
-            eng_words = list(filter(check_word_english_alpha, tokenized))
+            eng_words = list(filter(is_english_word_alpha, tokenized))
             ratios.append(len(eng_words) / len(tokenized))
             eng_words_each.append(get_bow_dataframe(eng_words))
         df['english_ratio'] = ratios
