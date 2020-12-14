9c325a8c507c12dabc29555261340456d0ba0393
T. Bunratta <tharathorn.bu@ku.th>
2020-12-14 20:38:52 +0700

    Add skeleton loader for SongListView

--- slap-vue/src/views/Songs.vue
+++ slap-vue/src/views/Songs.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-3" v-for="song in songs" :key="song.id">
-        <b-card>
-          <b-card-title>{{ song.title }}</b-card-title>
-          <b-card-text>
-            by {{ song.artists.join(", ") }}
+    <b-skeleton-wrapper :loading="loading">
+      <template #loading>
+        <b-skeleton width="85%"></b-skeleton>
+        <b-skeleton width="55%"></b-skeleton>
+        <b-skeleton width="70%"></b-skeleton>
+      </template>
+      <div class="row">
+          <div class="col-3" v-for="song in songs" :key="song.id">
+            <b-card>
+              <b-card-title>{{ song.title }}</b-card-title>
+              <b-card-text>
+                by {{ song.artists.join(", ") }}
 
-          </b-card-text>
-         <!--<b-card-text class="small text-muted">Last updated 3 mins ago</b-card-text>-->
-          <b-button :to="'/song/' + song.id" variant="primary">See details</b-button>
-        </b-card>
+              </b-card-text>
+              <!--<b-card-text class="small text-muted">Last updated 3 mins ago</b-card-text>-->
+              <b-button :to="'/song/' + song.id" variant="primary">See details</b-button>
+            </b-card>
+          </div>
       </div>
-    </div>
-    <b-pagination
-            v-model="currentPage"
-            :total-rows="totalCount"
-            :per-page="perPage"
-            aria-controls="my-table"
-            @change="onChangePage"
-    ></b-pagination>
+      <b-pagination
+              v-model="currentPage"
+              :total-rows="totalCount"
+              :per-page="perPage"
+              aria-controls="my-table"
+              @change="onChangePage"
+      ></b-pagination>
+    </b-skeleton-wrapper>
   </div>
 </template>
 
@@ -32,7 +39,8 @@ export default {
       songs: [],
       currentPage: 8,
       perPage: 25,
-      totalCount: 0
+      totalCount: 0,
+      loading: true
     };
   },
   watch: {
@@ -43,6 +51,7 @@ export default {
       this.fetchData()
     },
     fetchData() {
+      this.loading = true
       const api = new SongLyricsPopularitySlap.DefaultApi();
       api.slapFlaskPublicControllersSongsGetSongs({ ...this.$route.query }, (error, data) => {
         if (error) {
@@ -53,6 +62,7 @@ export default {
           this.perPage = 25
           this.totalCount = data['count']
           setTimeout(() => this.currentPage = this.$route.query.page)
+          this.loading = false
         }
       })
     }
