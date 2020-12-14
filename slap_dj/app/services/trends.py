b2580bbb614cc4c37a725f4ac2172e8dfe11960e
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-12-14 21:45:00 +0700

    Reformant trends.py

--- slap_dj/app/services/trends.py
+++ slap_dj/app/services/trends.py
@@ -1,10 +1,16 @@
+"""
+Service: Google Trends
+via pytrends
+"""
 from datetime import datetime
 from typing import Tuple
 
 import pandas as pd
-from pytrends.request import TrendReq
+
 import plotly.graph_objs as go
 from plotly.offline import plot
+
+from pytrends.request import TrendReq
 
 
 def retrieve_aggregate_song(song_name: str, mode: str = 'sum') -> Tuple[pd.DataFrame, datetime]:
@@ -15,17 +21,20 @@ def retrieve_aggregate_song(song_name: str, mode: str = 'sum') -> Tuple[pd.DataF
     df = pytrend.interest_over_time()
     df = df.sort_values(by=song_name)
     latest = df.sort_index().index.values[-1]
-    df = df.groupby([pd.DatetimeIndex(df.index.date).to_period("Y")]) \
-        .agg({song_name: [mode]})
+    df = df.groupby(
+        [pd.DatetimeIndex(df.index.date).to_period("Y")]
+    ).agg({song_name: [mode]})
     df = pd.DataFrame({'x': df.index.year, 'y': df[song_name, mode]})
     return df, datetime.utcfromtimestamp(latest.astype('O')/1e9)
 
 
 def plot_song_data_google_trends(song_name: str, mode: str = 'sum') -> Tuple[go.Figure, datetime]:
     df, latest = retrieve_aggregate_song(song_name, mode)
-    return go.Figure(data=[go.Scatter(x=df['x'], y=df['y'])],
-                     layout=go.Layout(title=go.layout.Title(text=f'\"{song_name}\" Keyword Trends - Cumulative Scores '
-                                                                 f'by year'))), latest
+    return go.Figure(
+        data=[go.Scatter(x=df['x'], y=df['y'])],
+        layout=go.Layout(
+            title=go.layout.Title(text=f'\"{song_name}\" Keyword Trends - Cumulative Scores by year')
+        )), latest
 
 
 def get_rendered_plot(figure: go.Figure) -> str:
