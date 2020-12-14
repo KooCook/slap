d74d90976188930ec5f3f003ab4906c290e68130
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-12-14 10:44:34 +0700

    Improve docs for pairwise() + Add fillvalue functionality

--- slap_dj/app/utils/iter.py
+++ slap_dj/app/utils/iter.py
@@ -1,15 +1,32 @@
-from itertools import chain, tee
-from typing import Iterable, overload, TypeVar, List
+from itertools import chain, tee, zip_longest
+from typing import Any, Iterable, Tuple, TypeVar, Union, overload
 
 __all__ = ['pairwise', 'remove_consecs']
 
 T = TypeVar('T')
 
 
-def pairwise(iterable: Iterable) -> zip:
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+@overload
+def pairwise(iterable: Iterable[T]) -> Iterable[Tuple[T, T]]: ...
+@overload
+def pairwise(iterable: Iterable[T], fillvalue: Any) -> Iterable[Tuple[T, T]]: ...
+def pairwise(iterable: Iterable[T], fillvalue: Any = None) -> Iterable[Tuple[T, Union[T, Any]]]:
+    """
+    Returns the iterable pairwise.
+    If `fillvalue` is not provided, does not return last value.
+
+    Args:
+        iterable: The iterable to make pairwise
+        fillvalue: The value to fill for the last value. Passed to zip_longest.
+
+    Examples:
+        s[0:10] -> (s0,s1), (s1,s2), (s2, s3), ..., (s8, s9)
+        s[0:10], None -> (s0,s1), (s1,s2), (s2, s3), ..., (s8, s9), (s9, None)
+    """
     a, b = tee(iterable)
     next(b, None)
+    if fillvalue is not None:
+        return zip_longest(a, b, fillvalue=fillvalue)
     return zip(a, b)
 
 
